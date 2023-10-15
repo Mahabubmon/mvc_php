1c200be163a7db7da9f4fe67e43a1af25f516ee5
Mahabubur Rahman <mahabubmon@gmail.com>
2023-10-15 14:33:22 +0600

    MVC_first

---
+++ mvc/index.php
@@ -0,0 +1,27 @@
+<?php include "inc/header.php"; ?>
+
+<?php 
+
+    include_once " system/libs/Main.php";
+    $main = new Main();
+    
+
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php include "inc/footer.php"; ?>
---
+++ mvc/system/libs/main.php
@@ -0,0 +1,12 @@
+<?php 
+
+    class Main{
+
+        function __construct(){
+            echo "I Am Mahabub from main class";
+        }
+        
+
+    }
+
+?>
