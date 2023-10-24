7c7145a0d8d6d09e4ac2e5cff79f570389369349
Mahabubur Rahman <mahabubmon@gmail.com>
2023-10-24 19:29:33 +0600

    router_done

--- src/Router.php
+++ src/Router.php
@@ -7,6 +7,18 @@ class Router{
     public function addRouter($router,$controller,$action){
         $this->router[$router] = ['controller'=> $controller, 'action'=>$action];
     }
+
+    public function dispatch($uri) {
+        if (array_key_exists($uri, $this->routes)) {
+            $controller = $this->routes[$uri]['controller'];
+            $action = $this->routes[$uri]['action'];
+
+            $controller = new $controller();
+            $controller->$action();
+        } else {
+            throw new \Exception("No route found for URI: $uri");
+        }
+    }
 }
 
 
